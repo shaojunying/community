40ada8741f5dd32b3203dfaf034780e418c84547
shaojunying <shaojunying@bupt.edu.cn>
2020-08-30 22:11:19 +0800

    完成了Mapper层对于User的增改查

---
+++ src/main/java/com/shao/community/dao/UserMapper.java
@@ -0,0 +1,31 @@
+package com.shao.community.dao;
+
+import com.shao.community.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Author: shao
+ * Date: 2020-08-19
+ * Time: 21:01
+ */
+@Mapper
+public interface UserMapper {
+
+    User selectById(int id);
+
+    // TODO: name和email不唯一,可能返回多个User对象
+    User selectByName(String name);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(int id, int status);
+
+    int updateHeader(int id, String headerUrl);
+
+    int updatePassword(int id, String password);
+
+    // TODO: 测试类没有写完
+
+}
---
+++ src/main/java/com/shao/community/entity/User.java
@@ -0,0 +1,142 @@
+package com.shao.community.entity;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Author: shao
+ * Date: 2020-08-19
+ * Time: 20:57
+ */
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String salt;
+    private String email;
+    private int type;
+    private int status;
+    private String activationCode;
+    private String headerUrl;
+    private Date createTime;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", salt='" + salt + '\'' +
+                ", email='" + email + '\'' +
+                ", type=" + type +
+                ", status=" + status +
+                ", activationCode='" + activationCode + '\'' +
+                ", headerUrl='" + headerUrl + '\'' +
+                ", createTime=" + createTime +
+                '}';
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
+
+    public String getHeaderUrl() {
+        return headerUrl;
+    }
+
+    public void setHeaderUrl(String headerUrl) {
+        this.headerUrl = headerUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getId() == user.getId() &&
+                getType() == user.getType() &&
+                getStatus() == user.getStatus() &&
+                Objects.equals(getUsername(), user.getUsername()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getSalt(), user.getSalt()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getActivationCode(), user.getActivationCode()) &&
+                Objects.equals(getHeaderUrl(), user.getHeaderUrl()) &&
+                Objects.equals(getCreateTime(), user.getCreateTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUsername(), getPassword(), getSalt(), getEmail(), getType(), getStatus(), getActivationCode(), getHeaderUrl(), getCreateTime());
+    }
+}
