ac1b11df6dc32593244dede7909c696cc97bdab8
shaojunying <shaojunying@bupt.edu.cn>
2020-09-04 22:18:22 +0800

    :white_check_mark: 测试了最新的日志配置

---
+++ src/test/java/com/shao/community/dao/LoggerTest.java
@@ -0,0 +1,32 @@
+package com.shao.community.dao;
+
+import com.shao.community.CommunityApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Author: shao
+ * Date: 2020-09-04
+ * Time: 22:11
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTest {
+    Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLog() {
+        logger.debug("debugLog");
+        logger.info("infoLog");
+        logger.warn("warnLog");
+        logger.error("error");
+    }
+
+
+}
