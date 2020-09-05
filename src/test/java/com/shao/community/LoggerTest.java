ba9d782635e629f92efd6d4c69de9b6451de6458
shaojunying <shaojunying@bupt.edu.cn>
2020-09-05 16:36:27 +0800

    :truck: 修改LoggerTest的存放路径

--- src/test/java/com/shao/community/LoggerTest.java
+++ src/test/java/com/shao/community/LoggerTest.java
@@ -1,6 +1,5 @@
-package com.shao.community.dao;
+package com.shao.community;
 
-import com.shao.community.CommunityApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
