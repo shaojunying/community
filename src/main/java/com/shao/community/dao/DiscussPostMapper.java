ed2e9d9887576a6161315324a1b42382389e1ebc
shaojunying <shaojunying@bupt.edu.cn>
2020-09-25 14:35:37 +0800

    :construction: implement the functionality of updating the comment's count of a discuss post in mapper layer

--- src/main/java/com/shao/community/dao/DiscussPostMapper.java
+++ src/main/java/com/shao/community/dao/DiscussPostMapper.java
@@ -6,8 +6,10 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 /**
+ * The interface Discuss post mapper.
+ *
  * @author shao
- * @date 2020-09-02 17:24
+ * @date 2020 -09-02 17:24
  */
 @Mapper
 public interface DiscussPostMapper {
@@ -19,7 +21,7 @@ public interface DiscussPostMapper {
      * @param userId 要查询的用户id,id为0时表示查询所有用户的帖子
      * @param offset 从第offset个帖子开始查询,之前的帖子跳过
      * @param limit  查询的最大数量
-     * @return 查询到的帖子列表
+     * @return 查询到的帖子列表 list
      */
     List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
 
@@ -27,7 +29,7 @@ public interface DiscussPostMapper {
      * 获取指定用户总帖子数
      *
      * @param userId 要查询的用户id,为0时表示所有用户
-     * @return 指定用户的总帖子数
+     * @return 指定用户的总帖子数 int
      */
     int selectDiscussPostsRows(int userId);
 
@@ -35,16 +37,25 @@ public interface DiscussPostMapper {
     /**
      * 插入一条帖子
      *
-     * @param discussPost
-     * @return
+     * @param discussPost the discuss post
+     * @return int
      */
     int insertDiscussPost(DiscussPost discussPost);
 
     /**
      * 根据帖子id查询帖子
      *
-     * @param id
-     * @return
+     * @param id the id
+     * @return discuss post
      */
     DiscussPost selectDiscussPostById(int id);
+
+    /**
+     * 更新指定帖子的总评论数量
+     *
+     * @param id           the id
+     * @param commentCount the comment count
+     * @return the int
+     */
+    int updateCommentCount(int id, int commentCount);
 }
